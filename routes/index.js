3a86dacee5360ca8c9cc6fa40a494ab326d2c381
David Lazaro <dlazaro7@gmail.com>
2022-10-16 23:49:57 +0200

    organizing films

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
 
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((response) => {
+      console.log(response);
+      res.render("movies.hbs", {
+        films: response,
+      });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
-router.get("/movies", (req, res, next)=> res.render("movies.hbs"));
+router.get("/movies/:filmId", (req, res, next) => {
+  let { filmId } = req.params;
+
+  Movie.findById(filmId)
+    .then((response) => {
+    let hours = response.showtimes.toString().replace(/,/g, " | ")  // "g" cambia el primer argumento entre "/" por el segundo.
+    
+    response.showtimes = hours
+            
+      console.log(hours);
+      
+      res.render("movies-details.hbs", {
+        details: response,
+      });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 module.exports = router;
